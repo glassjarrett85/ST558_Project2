8c522f2abb299fa4ab0a8b66821f55589c7b8ce6
Jarrett Glass <glassjarrett@gmail.com>
2025-10-28 22:00:52 -0400

    Additional updates: Adding more to the Shiny skeleton, and added the numeric_ and char_vars with some context.

--- GlassJ_Project2/app.R
+++ GlassJ_Project2/app.R
@@ -4,55 +4,78 @@
 # I have already authenticated this laptop, so when I am ready to deploy:
 # rsconnect::deployApp('path/to/your/app')
 
-# Topic of this project: EXOPLANETS. Unless Dr. Post disagrees, use the exoplanets dataset from kaggle.com.
+# Topic of this project: EXOPLANETS. Use the exoplanets dataset from kaggle.com.
 
 library(shiny)
+library(shinyalert)
 library(tidyverse)
+library(bslib)
 
-# Begin the skeleton for the project.
-df <- read.csv("GlassJ_Project2/exoplanetsdata.csv", header=TRUE) |>
+df <- read.csv("exoplanetsdata.csv", header=TRUE) |>
   mutate(across(c(pl_name, hostname, sy_snum, sy_pnum, discoverymethod, disc_year, disc_facility), factor))
 
 # Which columns do I want to keep? I don't think I need all 85 columns. 
 # Details of the columns:
 # https://exoplanetarchive.ipac.caltech.edu/docs/API_PS_columns.html
 
+# To start: I just copied text in directly from our last homework assignemnt, and I can tweak from here.
+
+# Number of stars: sy_num
+# Number of planets: sy_pnum
+# Number of moons: sy_mnum
+# YES OR NO: Planet orbits binary star system? : cb_flag
+
+# Planet discovery method: discoverymethod
+# Discovery year: disc_year
+
+source("helpers.R")
+
+# Define UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  h2("Exploration of Exoplanets"),
+  sidebarLayout(
+    sidebarPanel(
+      h2("Explore Exoplanets"),
+      
+      selectInput("corr_x", "First Numeric Variable", numeric_vars[-1]),
+      selectInput("corr_y", "Second Numeric Variable", numeric_vars),
+      
+      h2("Choose a subset of the data:"),
+      
+      # Options to select for Household Language
+      radioButtons("hhl_corr","Household Language", choiceNames=c("All","English only","Spanish", "Other"),
+                   choiceValues=c("all","english","spanish","other")),
+      
+      # Options to select for SNAP Recipients
+      radioButtons("fs_corr","SNAP Recipient", choiceNames=c("All", "Yes", "No"),
+                   choiceValues=c("all","yes","no")),
+      
+      # Options to select for Educational Attainment
+      radioButtons("schl_corr","Educational attainment",
+                   choiceNames=c("All","High School not Completed", "High School or GED", "College Degree"),
+                   choiceValues=c("all", "no_hs", "hs", "college")),
+      
+      h2("Select a Sample Size"),
+      sliderInput("corr_n", "", value=20, min=20, max=500),
+      actionButton("corr_sample","Get a Sample!")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Info", mainPanel(
+          h1("This is the Main Panel"),
+          "I can put whatever I want",
+          br(),
+          h2("To add later.")
+        )),
+        tabPanel("Stars"),
+        tabPanel("Planets")
+      )
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
+server <- function(input, output, session) { }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
---
+++ GlassJ_Project2/helpers.R
@@ -0,0 +1,38 @@
+numeric_vars <- c(
+  "facility_type",    # 
+  "pl_controv_flag",  # lag indicating whether the confirmation status of a planet has been questioned in the published literature (1=yes, 0=no)
+  "pl_orbper",        # Time the planet takes to make a complete orbit around the host star or system
+  "pl_orbsmax",       # The longest radius of an elliptic orbit, or, for exoplanets detected via gravitational microlensing or direct imaging, the projected separation in the plane of the sky
+  "pl_rade",          # Length of a line segment from the center of the planet to its surface, measured in units of radius of the Earth
+  "pl_radj",          # Length of a line segment from the center of the planet to its surface, measured in units of radius of Jupiter
+  "pl_bmasse",        # Best planet mass estimate available, in order of preference: Mass, M*sin(i)/sin(i), or M*sin(i), depending on availability, and measured in Earth masses
+  "pl_bmassj",        # Best planet mass estimate available, in order of preference: Mass, M*sin(i)/sin(i), or M*sin(i), depending on availability, and measured in Jupiter masses
+  "pl_orbeccen",      # Amount by which the orbit of the planet deviates from a perfect circle
+  "pl_insol",         # Insolation flux is another way to give the equilibrium temperature. It's given in units relative to those measured for the Earth from the Sun.
+  "pl_eqt",           # The equilibrium temperature of the planet as modeled by a black body heated only by its host star, or for directly imaged planets, the effective temperature of the planet required to match the measured luminosity if the planet were a black body
+  "ttv_flag",         # Flag indicating if the planet orbit exhibits transit timing variations from another planet in the system (1=yes, 0=no).
+  "st_teff",          # Temperature of the star as modeled by a black body emitting the same total amount of electromagnetic radiation
+  "st_rad",           # Length of a line segment from the center of the star to its surface, measured in units of radius of the Sun
+  "st_mass",          # Amount of matter contained in the star, measured in units of masses of the Sun
+  "st_met",           # Measurement of the metal content of the photosphere of the star as compared to the hydrogen content
+  "st_logg",          # Gravitational acceleration experienced at the stellar surface
+  "ra",               # Right Ascension of the planetary system in decimal degrees
+  "dec",              # Declination of the planetary system in decimal degrees
+  "sy_dist",          # Distance to the planetary system in units of parsecs
+  "sy_vmag",          # Brightness of the host star as measured using the V (Johnson) band in units of magnitudes
+  "sy_kmag",          # Brightness of the host star as measured using the K (2MASS) band in units of magnitudes
+  "sy_gaiamag"        # Brightness of the host star as measuring using the Gaia band in units of magnitudes. Objects matched to Gaia using the Hipparcos or 2MASS IDs provided in Gaia DR2.
+)
+char_vars <- c(
+  "pl_name",          # Planet name most commonly used in the literature
+  "hostname",         # Stellar name most commonly used in the literature
+  "sy_snum",          # Number of gravitationally bound stars in the planetary system
+  "discoverymethod",  # Method by which the planet was first identified
+  "disc_year",        # Year the planet was discovered
+  "disc_facility",    # Name of facility of planet discovery observations
+  "pl_bmassprov",     # Provenance of the measurement of the best mass. Options are: Mass, M*sin(i)/sin(i), and M*sini
+  "st_spectype",      # Classification of the star based on their spectral characteristics following the Morgan-Keenan system
+  "st_metratio",      # Ratio for the Metallicity Value ([Fe/H] denotes iron abundance, [M/H] refers to a general metal content)
+  "rastr",            # Right Ascension of the planetary system in sexagesimal format
+  "decstr"            # Declination of the planetary system in sexagesimal notation
+)
